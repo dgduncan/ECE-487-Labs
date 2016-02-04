ce9ee703ad8cee461c42578ae5785fc0a58fe15d
Derek Duncan <derekgduncan@gmail.com>
2016-02-03 19:30:26 -0600

    added pin initialization

--- Lab 2/main/Lab2/Lab2.ino
+++ Lab 2/main/Lab2/Lab2.ino
@@ -1,6 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Derek Duncan
+ * CWID: 11387683
+ * ECE 486
+ * Lab 2
+ * 2/03/2016
+ */
 
+void setup() 
+{
+  /*Initalize pins to be used to light LEDs*/
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  /****************************************/
 }
 
 void loop() {
