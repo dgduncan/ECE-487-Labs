e2406177942f13d548611a1154ffd811d750d0c4
Derek Duncan <derekgduncan@gmail.com>
2016-02-08 15:38:48 -0600

    added debounce to project along with blinking LEDs

--- Lab 2/main/Lab2/Lab2.ino
+++ Lab 2/main/Lab2/Lab2.ino
@@ -7,16 +7,29 @@
  * Lab 2
  * 2/03/2016
  */
-
+ 
 /*Declare and Initalize the SegmentDisplay class with the appropriate pins*/
 SegmentDisplay segmentDisplay(2, 3, 4, 5, 6, 7, 8, 9);
 /*************************************************************************/
 
+int reading;
+int buttonState = HIGH;
+int lastDebounceTime;
+int lastButtonReading = HIGH;
+int lastButtonState = HIGH;
+int userInteractions = 0;
+int ledBlinkingFlag = LOW;
+
+
+
+
 void setup() 
 {
-  /*Initialize pin13 LED*/
+  /*Initialize pin13 LED and pin12 as output*/
   pinMode(13, OUTPUT);
-  /**********************/
+  pinMode(12, OUTPUT);
+  pinMode(11, INPUT); 
+  /******************************************/
   
   /*Set the baud rate, begin serial, and print intial prompt*/
   Serial.begin(9600);
@@ -26,39 +39,105 @@ void setup()
 void loop() 
 {
 
-  segmentDisplay.testDisplay();
+  checkReading();
+
+  /*if(reading != lastButtonState)
+  {
+    Serial.println("debounce reset");
+    lastDebounceTime = millis();
+  }
+
+  if((millis() - lastDebounceTime) > 50)
+  {
+
+    if(reading != buttonState)
+    {
+      Serial.println("The button state changed");
+      buttonState = reading;
+
+      if(buttonState == LOW)
+      {
+        ledBlinkingFlag = 1;
+      }
+    }
+
+    
+  }
+
+  if(buttonState == HIGH)
+    {
+      ledBlinkingFlag = 0;
+    }
+
+  lastButtonState = reading;
+
+  ledBlinking(ledBlinkingFlag);*/
+  
+
+
   
 
   
 }
 
-void ledBlinking(int inputHighLow)
+void checkReading()
 {
-  while(inputHighLow != 0)
-    {
-      /*Turn onboard LED on and off-board led off for 2 seconds*/
-      digitalWrite(13, HIGH);
-      digitalWrite(12, LOW);
-      delay(2000);
-      /*********************************************************/
-      
-      /*Do the opposite of above for 1 second*/
-      digitalWrite(13, LOW);
-      digitalWrite(12, HIGH);
-      delay(1000);
-      /***************************************/
-      
-      /*Check to see if user has entered anything*/
-      inputHighLow = digitalRead(2);
-      /*******************************************/
-    }
+  /*Get the current value on pin11*/
+  reading = digitalRead(11);
+  /********************************/
 
-    /*Shut off LEDs*/
+  /*If the state of the button is changing, reset the debounceTimer*/
+  if(reading != lastButtonReading)
+  {
+    lastDebounceTime = millis();
+  }
+  /****************************************************************/
+
+  
+  if((millis() - lastDebounceTime) > 100)
+  {
+    
+    
+    if(reading != buttonState)
+    {
+      Serial.println("Button state has officially changed");
+      buttonState = reading;
+    }
+    
+    blinkLED();
+  }
+
+  lastButtonReading = reading;
+  
+  
+
+  
+  
+}
+
+void blinkLED()
+{
+  if(buttonState == LOW)
+  {
+    Serial.println("Start blink cycle");
+    /*Turn onboard LED on and off-board led off for 2 seconds*/
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    delay(1000);
+    /*********************************************************/
+  
+    /*Do the opposite of above for 1 second*/
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    delay(1000);
+    /***************************************/
+  }
+
+  else
+  {
     digitalWrite(13, LOW);
     digitalWrite(12, LOW);
-    /***************/
-    
-    /*Notify User the LEDs are turned off*/
-    Serial.println("LEDs are now off");
-    /************************************/
+  }
+
+
 }
