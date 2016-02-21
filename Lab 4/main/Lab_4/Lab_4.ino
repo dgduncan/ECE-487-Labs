79bbf7a725b8c2d6e3d71c8143e1f9633475d756
Derek Duncan <derekgduncan@gmail.com>
2016-02-21 14:55:20 -0600

    added comments for ease of understanding

--- Lab 4/main/Lab_4/Lab_4.ino
+++ Lab 4/main/Lab_4/Lab_4.ino
@@ -8,30 +8,33 @@
 
 void setup() 
 {
+  /*Open Serial to begin communication and prompt user for input*/
   Serial.begin(9600);
   Serial.println("Please input the character 'c' into the serial input, other inputs will be rejected");
-  
-  
-
 }
 
 void loop() 
 {
-  if(checkSerial(); == 'c')
+  /*Only act if a 'c' is read on the serial*/
+  if(checkSerial() == 'c')
   {
+    /*Begin the 30 analog reads*/
     beingAnalogReads();
   }
-  
-
 }
 
 void beginAnalogReads()
 {
+  /*The analog data that will be read*/
   int analogData;
+
+  /*The total time in milliseconds that it took to read from analog pin*/
   int readTime;
+
+  /*The time in millis when the read was first begun*/
   int startOfRead;
   
-  for(int i = 0; i <= 30; i++)
+  for(int i = 0; i < 30; i++)
   {
     startOfRead = millis();
     
@@ -40,9 +43,12 @@ void beginAnalogReads()
     readTime = millis() - startOfRead;
 
     displayResults();
+
+    delay(500-readTime);
   }
 }
 
+/*Displaying data in accordance to lab parameters*/
 void displayResults(int readSequence, int analogData, int readTime)
 {
   Serial.print("#");
@@ -60,38 +66,30 @@ char checkSerial()
 {
   /*Initialized character y */
   char firstCharacter;
-  /*************************/
 
   /*Checks if there are more than 2 characters in the stream*/
   if(Serial.available() > 2)
   {
     /*Notifies user that they messed up*/
     Serial.println("Please only input 1 character");
-    /**********************************/
 
     /*Enters method to clear serial input stream*/
     clearSerialInput();
-    /*******************************************/
 
     /*Return meaningless character*/
     return 'a';
-    /******************************/
   }
-  /**********************************************************/
 
   else
   {
     /*Read first byte of the stream*/
     firstCharacter = Serial.read();
-    /*******************************/
 
     /*Enters method to clear serial input stream*/
     clearSerialInput();
-    /********************************************/
 
     /*Return character containing first byte of the stream*/
     return firstCharacter;
-    /****************************************************/
   }
 }
 
@@ -102,9 +100,7 @@ void clearSerialInput()
   {
     Serial.read();
   }
-  /*********************************************************/
 
   /*Apparently there is some delay in clearing the data, so delay prevents a double run through of the code*/
-  delay(100);
-  /*********************************************************************************************************/
+  delay(50);
 }
